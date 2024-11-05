a8d0a96192a7e9adf59c5bfa5ad701fd5d1d3b8c
Francisco Gomez Salaverri <francisco@gomezsalaverri.com>
2024-11-05 10:40:02 -0300

    first commit

---
+++ src/main/java/aed/utils/TrasladoComparator.java
@@ -0,0 +1,14 @@
+package aed.utils;
+
+import aed.Traslado;
+
+import java.util.Comparator;
+
+public class TrasladoComparator implements Comparator<Traslado> {
+
+    @Override
+    public int compare(Traslado t1, Traslado t2) {
+        // Comparar traslados según rentabilidad o antigüedad
+        return 0;
+    }
+}
